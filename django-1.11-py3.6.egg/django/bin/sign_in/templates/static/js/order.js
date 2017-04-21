4d5b8bf3ac39ff66eb82e6e34b2e5b72a8c7b69d
niyaou <niyaou@163.com>
2017-04-22 03:48:25 +0800

    add goodlist and distination pick

--- django-1.11-py3.6.egg/django/bin/sign_in/templates/static/js/order.js
+++ django-1.11-py3.6.egg/django/bin/sign_in/templates/static/js/order.js
@@ -36,35 +36,84 @@ function async_table() {
             //alert(JSON.stringify(data));
             console.info(data);
 
-            var jsonData = [{caller: 'fdas', imsi: '321', imei: 'fffaa', osid: 'bbd1'}, {
-                caller: 'fdas',
-                imsi: '32fdasfdsafdsfdsafdsfdsafdasfdsa1',
-                imei: 'ffffdsaaa',
-                osid: 'bbdfffda1'
-            }, {caller: 'fdas', imsi: '321', imei: 'fffaa', osid: 'bbd1'}];
+            var jsonData = [{"id": 1, "name": "牛肉", "price": 100, "status": 1, "weight": 250}, {
+                "id": 2,
+                "description": "好吃fda发货的沙发回去我答复哈斯卡",
+                "name": "冷吃兔",
+                "price": 250,
+                "status": 1,
+                "weight": 200
+            }, {"id": 3, "description": "幅度萨芬毫无起伏较大斯科拉jfk", "name": "鹌鹑蛋", "price": 140, "status": 1, "weight": 200},
+                {
+                    "id": 4,
+                    "name": "海带",
+                    "price": 130,
+                    "status": 1,
+                    "weight": 200
+                }]
 
-            var table = $("<table class='table' id='goodlist'>       <caption>基本的表格布局</caption>            <thead>            <tr>            <th>名称</th>            <th>城市</th>            </tr>            </thead>            <tbody>");
-            table.appendTo($("#createtable"));
+
+            //var table = $('<caption>基本的表格布局</caption>   <thead>                <tr>                <th>名称</th>                <th>城市</th>                </tr>                </thead>    ');
+
+            var tbody = $("<tbody></tbody> ");
             for (one in jsonData) {
-                var tr = $("<tr></tr>");
-                tr.appendTo(table);
+                var tr = $("<tr></tr> ");
+
+                var td1 = $('<td class="col-xs-10"> <div> <div class="panel panel-default">                    <div class="panel-heading">                    <h4 class="panel-title">                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo' + one + '" > '+jsonData[one].name+'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;￥'+jsonData[one].price+'  </a>     </h4>     </div>');
+               if(jsonData[one].description)
+                td1.append($(( "<div id='collapseTwo" + one + "' class='panel-collapse collapse'>                <div class='panel-body'>      " + jsonData[one].description + "                          </div>                </div>   </div></div> </td>")));
 
 
-                var td = $(" <td class='center-block' style='width:100%'>" + "<div class='row'>                        <div class='panel panel-default col-xs-10'>                        <div class='panel-heading'>                        <h4 class='panel-title'>                       <a data-toggle='collapse' data-parent='#accordion'                    href='#"+one+"'>                        点击我进行展开，再次点击我进行折叠。第 2 部分                    </a>                    </h4>                    </div>"
-                    + "<div id='"+one+"' class='panel-collapse collapse'>                <div class='panel-body'>      "+jsonData[one].imsi+"                          </div>                </div>   </div>"
-                    +"  <div class='col-xs-1'><button>1</button></div>                </div>"
-                    +"</td>");
+                var td = $(( " <td>  <div class='col-xs-1' >   <img src='static/img/plus.png' style='height:2.5vh;'id='plus" + one + "'/>      <p id='count" + one + "'>0</p>  <img src='static/img/minus.png' style='height:2.5vh;'id='minus" + one + "' /> </div>   </div>"));
+                td.append($(( "</td>")));
+
+                td1.appendTo(tr);
                 td.appendTo(tr);
+                tr.appendTo(tbody);
+
 
             }
+            //tbody.appendTo(table);
+            ( $("#goodlist")).append(tbody);
+            for (one in jsonData) {
+                $("#minus" + one).click(function () {
+                    plus_total(this.id.substr(this.id.length - 1, 1), -1);
+                });
+                $("#plus" + one).click(function () {
+                    plus_total(this.id.substr(this.id.length - 1, 1), 1);
+                });
+            }
 
-            $("#createtable").append("</table>");
 
-
+            //alert($("#goodlist").html());
         },
         error: function (xhr, status, error) {
             console.info(status + xhr + error);
             //alert(error);
         }
     })
+}
+
+function plus_total(index, type) {
+    var original = parseInt($("#count" + index).text());
+    var total = original + type;
+    if (total < 0)
+        return;
+    var id = "#count" + index;
+    console.info(id + "   " + total);
+    $(id).text(total);
+}
+
+function submit_order(){
+  var address=getValue("provence")+getValue("city")+getValue("zone")+getValue("distination")+
+      "       姓名:"+getValue("name")+"    电话:"+getValue("phone");
+    alert(address);
+}
+function getValue(id){
+    if($("#"+id+" option:selected").text()){
+        return $("#"+id+" option:selected").text();
+    }else{
+        return $("#"+id).val();
+    }
+
 }
